59a59ff3004de6aa6cfe27c6c2716e19ab656974
ohad1s <92723105+ohad1s@users.noreply.github.com>
2021-11-03 15:12:18 +0200

    NumClass header

---
+++ NumClass.h
@@ -0,0 +1,27 @@
+//------------------------------------------------
+// Created by Ohad Shirazi on 29/10/2021.
+// NumClass header file
+//------------------------------------------------
+
+#ifndef EX1_NUMCLASS_H
+#define EX1_NUMCLASS_H
+#endif //EX1_NUMCLASS_H
+
+int isPrime(int n);/* will return if a number is prime*/
+int isStrong(int n);/*Strong number is a special number whose sum of the factorial of digits is equal to the original number. For Example: 145 is strong number
+1! + 4! + 5! = 145
+*/
+
+
+int isPalindrome(int n); /* will return if a number is a palindrome */
+int isArmstrong(int n);/* will return if a number is Armstrong number
+An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+*/
+
+
+int isPalindrome(int n); /* will return if a number is a palindrome */
+int isArmstrong(int n); /* will return if a number is Armstrong number
+An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+*/
